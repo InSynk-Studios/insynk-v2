2126f477122ea968a5aa3dcd3ef6fc7009d25b28
Manish <manishsdlp222@gmail.com>
2023-02-26 12:15:04 +0530

    feat(Hero.jsx): increase heading animation speed and add fade effect in illustraions

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "animate.css";
 import { useState } from "react";
 import Typewriter from "typewriter-effect";
 import Image from "next/image";
@@ -21,6 +22,7 @@ export const Hero = () => {
         <Typewriter
           onInit={(typewriter) => {
             typewriter
+              .changeDelay(50)
               .typeString(
                 `We are the reliable tech <span style="color: #FDA567;">partner</span> that you are looking for`
               )
@@ -28,8 +30,10 @@ export const Hero = () => {
                 setDisplayTech(true);
               })
               .pauseFor(2500)
+              .changeDeleteSpeed(30)
               .deleteChars(46);
             typewriter
+              .changeDelay(50)
               .typeString(
                 `<span style="color: #FDA567;">collaborative</span> team you need`
               )
@@ -37,8 +41,10 @@ export const Hero = () => {
                 setDisplayCreativity(true);
               })
               .pauseFor(2500)
+              .changeDeleteSpeed(30)
               .deleteChars(35);
             typewriter
+              .changeDelay(50)
               .typeString(
                 `aim to thrive on your <span style="color: #FDA567;">trust</span>`
               )
@@ -46,8 +52,10 @@ export const Hero = () => {
                 setDisplayBusinessDriven(true);
               })
               .pauseFor(2500)
+              .changeDeleteSpeed(30)
               .deleteAll();
             typewriter
+              .changeDelay(50)
               .typeString(
                 `Let’s build <span style="color: #FDA567;">together</span> !`
               )
@@ -62,27 +70,51 @@ export const Hero = () => {
           }}
         />
       </div>
-      <div className="row-span-2 grid h-3/5 grid-cols-4 place-items-center">
-        <div className="row-span-1">
+      <div className="grid h-3/5 grid-cols-4 w-full">
+        <div
+          className={`place-self-center xl:-mr-12 ${
+            displayCreativity && "animate__animated animate__fadeInRight"
+          }`}
+        >
           {displayCreativity ? (
             <Image priority src={Creativity} alt="" />
           ) : null}
         </div>
-        <div className="row-span-2 col-span-2">
+        <div className="row-span-2 col-span-2 place-self-center">
           <Image priority src={HeroIllustration} alt="" />
           {displayText ? (
-            <h2 className="font-caveat-700 text-3xl">Insynk studios</h2>
+            <h2 className="font-caveat-700 text-3xl animate__animated animate__fadeUpBig">
+              Insynk studios
+            </h2>
           ) : null}
         </div>
-        <div className="row-span-1">
+        <div
+          className={`row-span-1 place-self-end ${
+            displayBusinessDriven && "animate__animated animate__fadeInDown"
+          }`}
+        >
           {displayBusinessDriven ? (
             <Image priority src={BusinessDriven} alt="" />
           ) : null}
         </div>
-        <div>{displayTech ? <Image priority src={Tech} alt="" /> : null}</div>
-        <div className="row-span-1">
+        <div
+          className={`place-self-end xl:-mr-12 ${
+            displayTech && "animate__animated animate__fadeInLeft"
+          }`}
+        >
+          {displayTech ? <Image priority src={Tech} alt="" /> : null}
+        </div>
+        <div
+          className={`place-self-center xl:-ml-40 ${
+            displayBestInIndustry && "animate__animated animate__fadeInLeft"
+          }`}
+        >
           {displayBestInIndustry ? (
-            <Image priority src={BestInIndustry} alt="" />
+            <Image
+              priority
+              src={BestInIndustry}
+              alt=""
+            />
           ) : null}
         </div>
       </div>
